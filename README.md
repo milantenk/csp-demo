ea998c328e900396a786988b331c0f220aabcba9
Milan Tenk <tenkmilan@gmail.com>
2022-04-04 19:45:09 +0200

    CSP demo added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Content Security Policy Demo Starter Files
+Content Security Policy Demo Files for education purpose.
+
+## Get Started
+ * `npm install`
+ * `npm run start`
+The app can be opened here: http://localhost:5500/
+
+## Detailed description
+Tutorial can be found here: https://www.digitalocean.com/community/tutorials/how-to-secure-node-js-applications-with-a-content-security-policy
+
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
 
